a4acf6ecfce865160e1d8c92589430c3f1ba6486
Ignarius-Vow <fellowfbiagent@gmail.com>
2024-12-13 17:50:53 -0700

    Working on template issue

--- _server/_server/settings.py
+++ _server/_server/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 # DONT USE THE STATIC URL WHEN IN DEBUG MODE
 STATIC_URL = 'static/' if not DEBUG else "__UNUSED__/"
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
